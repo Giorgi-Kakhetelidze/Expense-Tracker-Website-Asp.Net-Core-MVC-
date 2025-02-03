4a78d8925b77dd5b599e64633bed1688c5819228
Kaxeta007 <giorgikakhetelidze73@gmail.com>
2025-02-03 13:38:35 +0400

    Add project files.

---
+++ Expense Tracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        public DbSet<Transaction> TransactionSet { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
